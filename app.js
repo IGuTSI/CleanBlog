684dde64f6c64831b71dddb7d0eb01d6fcfaab8a
IGuTSI <fatihkarakullukcu35@gmail.com>
2025-07-19 23:44:37 +0300

    Statcik Files Uploaded

--- app.js
+++ app.js
@@ -1,3 +1,29 @@
-const fn = function () {
-  console.log('Funciton working');
+const express = require('express');
+
+const path = require('path');
+
+const app = express();
+
+const myLogger = (req, res, next) => {
+  console.log('Middleware Log 1');
+  next(); //middleware'ler sırayla çalışır o yüzden next çağırtırız.
 };
+
+const myLogger2 = (req, res, next) => {
+  console.log('Middleware Log 2');
+  next(); //middleware'ler sırayla çalışır o yüzden next çağırtırız.
+};
+
+app.use(express.static('public')); // public içerisindeki statik dosyaları kullan
+
+app.use(myLogger);
+app.use(myLogger2);
+
+app.get('/', (req, res) => {
+  res.sendFile(path.resolve(__dirname, 'temp/index.html'));
+});
+
+const port = 4500;
+app.listen(port, () => {
+  console.log(`Sunucu ${port}'unda başlatıldı....`);
+});
